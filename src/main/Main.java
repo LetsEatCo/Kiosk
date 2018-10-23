f0dcaedc0e4df3e1757c400239cd36b32ab036b5
fletcher0702 <abediernelson@live.fr>
2018-10-23 16:22:14 +0200

    feat: ✨ admin kiosk registration

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,22 +1,21 @@
 package main;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
-import main.fr.esgi.kiosk.routes.Stores;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
 
-        String email = "toto@toto.fr";
-        String password = "test";
-        Stores.login(email,password);
 
-//        Parent root = FXMLLoader.load(getClass().getResource("fr/esgi/kiosk/views/home.fxml"));
-//        primaryStage.setTitle("Let\'s Eat");
-//        primaryStage.setScene(new Scene(root, 300, 275));
-//        primaryStage.show();
+        Parent root = FXMLLoader.load(getClass().getResource("fr/esgi/kiosk/views/home.fxml"));
+        primaryStage.setTitle("Let\'s Eat");
+        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.show();
     }
 
 
---
+++ src/main/fr/esgi/kiosk/helpers/StageHelper.java
@@ -0,0 +1,42 @@
+package main.fr.esgi.kiosk.helpers;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import java.io.IOException;
+
+public class StageHelper {
+
+
+    public static void loadWindow(String path, String title, Stage rootStage){
+
+        try {
+            Parent root = FXMLLoader.load(StageHelper.class.getResource(path));
+
+            rootStage.setTitle(title);
+            Scene myScene =new Scene(root);
+
+            rootStage.setScene(myScene);
+
+            rootStage.show();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
+    public static void returnHome(ActionEvent event) {
+
+        String home ="/main/fr/esgi/kiosk/views/home.fxml";
+
+        Stage rootStage = (Stage)((Node)event.getSource()).getScene().getWindow();
+
+        loadWindow(home , "Home", rootStage);
+
+    }
+
+}
