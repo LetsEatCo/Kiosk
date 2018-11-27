46fa402c4deca6b33211a2c53c99b452bd11d307
ABEDIER <abediernelson@live.fr>
2018-11-27 12:45:28 +0100

    feat: ✨ Generify HttpPost request

--- src/main/fr/esgi/kiosk/helpers/HttpHelper.java
+++ src/main/fr/esgi/kiosk/helpers/HttpHelper.java
@@ -5,6 +5,7 @@ import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.json.simple.parser.ParseException;
@@ -48,12 +49,12 @@ public class HttpHelper {
         return body;
     }
 
-    public static Object httpPostRequest(String url, List<NameValuePair> credentials) throws IOException, ParseException {
+    public static Object httpPostRequest(String url, StringEntity credentials) throws IOException, ParseException {
 
         CloseableHttpClient httpClient = HttpClients.createDefault();
 
         HttpPost httpPost = new HttpPost(url);
-        httpPost.setEntity(new UrlEncodedFormEntity(credentials));
+        httpPost.setEntity(credentials);
         CloseableHttpResponse response = httpClient.execute(httpPost);
 
         return JsonHelper.getResponseBody(response);
