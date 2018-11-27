4891101dcc912e0d27dfbf2a526b2eca7203068c
ABEDIER <abediernelson@live.fr>
2018-11-27 12:46:13 +0100

    feat: ✨ Processing order

--- src/main/fr/esgi/kiosk/models/CartElement.java
+++ src/main/fr/esgi/kiosk/models/CartElement.java
@@ -4,13 +4,21 @@ import java.util.HashSet;
 
 class CartElement {
 
-    private String uuid;
+    private String mealUuid;
+    private String productUuid;
     private int quantity;
     private HashSet<String> optionUuids;
 
-    CartElement(String uuid, int quantity, HashSet<String> optionUuids) {
-        this.uuid = uuid;
+
+    public <T extends RessourceElementProduct> CartElement(String uuid, int quantity, HashSet<String> optionUuids, T productElement) {
+
+
+        if(productElement instanceof Meal){
+            this.mealUuid = uuid;
+        }else this.productUuid = uuid;
+
         this.quantity = quantity;
         this.optionUuids = optionUuids;
+
     }
 }
--- src/main/fr/esgi/kiosk/models/Order.java
+++ src/main/fr/esgi/kiosk/models/Order.java
@@ -11,12 +11,24 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Properties;
 
-public class Order extends ArrayList<CartElement> {
+public class Order{
 
-    private boolean isTakeAway;
+    private boolean isTakeAway = false;
+    private boolean isEatIn = false;
+    private String storeUuid;
+    private ArrayList<CartElement> cart = new ArrayList<>();
+
 
     public void setTakeAway(boolean takeAway) {
         isTakeAway = takeAway;
+    }
+
+    public void setEatIn(boolean eatIn) {
+        isEatIn = eatIn;
+    }
+
+    public void setStoreUuid(String storeUuid) {
+        this.storeUuid = storeUuid;
     }
 
     public <T extends RessourceElementProduct> void process(Cart<T> cart) {
@@ -25,12 +37,12 @@ public class Order extends ArrayList<CartElement> {
 
             String uuid = productElement.getUuid();
             int quantity = (int)productElement.getQuantity();
-            CartElement cartElement = new CartElement(uuid,quantity, productElement.getOptionsUuids());
-            this.add(cartElement);
+            CartElement cartElement = new CartElement(uuid,quantity, productElement.getOptionsUuids(), productElement);
+            this.cart.add(cartElement);
 
         }
 
-        //processOrder();
+        processOrder();
     }
 
     private void processOrder()  {
@@ -42,9 +54,11 @@ public class Order extends ArrayList<CartElement> {
             Properties routes = credentialsHelper.getRoutes();
             String orderUrl = routes.getProperty("order");
             params = new StringEntity(gson.toJson(this));
-//            HttpHelper.httpPostRequest(orderUrl, params);
+            params.setContentType("application/json");
+            HttpHelper.httpPostRequest(orderUrl, params);
+            this.cart.clear();
 
-        } catch (IOException e) {
+        } catch (IOException | ParseException e) {
             e.printStackTrace();
         }
 
